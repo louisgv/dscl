a77734f697fef6292e926b2665da0e4eb3a6fc18
nettijoe96 <nettijoe@gmail.com>
2017-06-11 03:10:56 -0400

    updated send_transaction.js

--- src/client/scripts/SendButton/send_transaction.js
+++ src/client/scripts/SendButton/send_transaction.js
@@ -1,12 +1,21 @@
 import {bonds} from 'oo7-parity';
+var DH = require("./DHUtils")
 
 // TODO: Add export like this: `export function makeContractObject ...`
-function makeContractObject(contractAddress){
-  const ABI = [{"enter fully ABI definition json object here"}] //https://github.com/paritytech/parity/wiki/Tutorial-Part-6
-  contractObj = bonds.makeCountract(contractAddress, ABI)
+export function initalRequest(otherAddress) {
+  contractAddress = ... //TODO: add contract address after deployed
+  var abi = [{"enter fully ABI definition json object here"}] //https://github.com/paritytech/parity/wiki/Tutorial-Part-6
+  var contractObj = makeContractAddress(contractAddress);
+  var myDH = new DH();
+  gA = myDH.ga();
+  sendCommunicationRequest(contractObj, otherAddress, gA)
+}
+
+function makeContractObject(contractAddress, abi){
+  contractObj = bonds.makeCountract(contractAddress, abi)
   return contractObj
 }
 
 function sendCommunicationRequest(contractObj, otherAddress, gA) {
-    contractObj.initCommunication(otherAddress, gA) //gA is the first diffie helman (G^a)
+    contractObj.requestCommunication(otherAddress, gA) //gA is the first diffie helman (G^a)
 }
